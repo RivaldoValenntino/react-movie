abff11bbee41ae16178b0d2f5b78289b45455c9b
Rivaldo Valentino <jefry.kotak1908@gmail.com>
2023-10-01 14:01:46 +0700

    fix buggg

--- src/Search.jsx
+++ src/Search.jsx
@@ -19,11 +19,13 @@ const SearchPage = () => {
             <Navbar title="MovieApp" />
             <div className="search-page w-full flex flex-col justify-center items-center z-[999]">
                 <section className="mt-20">
-                    <h1 className="text-white lg:text-5xl text-center text-3xl font-bold" style={{ fontFamily: 'Poppins', textShadow: '2px 2px 3px #000000', letterSpacing: '2px' }}>Welcome to MovieMania</h1>
+                    <div className="pt-10">
+                        <h1 className="text-white lg:text-5xl text-center text-3xl font-bold" style={{ fontFamily: 'Poppins', textShadow: '2px 2px 3px #000000', letterSpacing: '2px' }}>Welcome to MovieMania</h1>
                     <p className="text-white text-center lg:text-4xl text-xl font-semibold" style={{ fontFamily: 'Poppins', textShadow: '2px 2px 3px #000000', letterSpacing: '2px' }}>Discover every Movie and TV Show</p>
                     <p className="text-white text-center lg:text-4xl font-semibold" style={{ fontFamily: 'Poppins', textShadow: '2px 2px 3px #000000', letterSpacing: '2px' }}>From Trending and Popular</p>
+                    </div>
                 </section>
-                <section className="w-3/4 lg:w-full md:w-full mt-20">
+                <section className="w-3/4 lg:w-full md:w-full mt-36">
                     <div className="mb-5">
                         <div className="relative mb-4 pt-10 flex w-full justify-center items-center">
                             <span
@@ -53,7 +55,7 @@ const SearchPage = () => {
                     </div>
                 </section>
             </div>
-            <div className="container flex flex-col items-center">
+            <div className="container flex flex-col items-center pb-36 mb-40">
 
                 {moviesSearch.length > 0 && (
                     <p className="text-center my-8 text-white">Search Result For "{moviesSearch[0].title}"</p>
