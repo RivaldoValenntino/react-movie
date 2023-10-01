3b64ef5891b31bbb5a72aea01e1763d051f5b7e1
Rivaldo Valentino <jefry.kotak1908@gmail.com>
2023-10-01 18:35:25 +0700

    fix

--- src/Movie.jsx
+++ src/Movie.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import Navbar from "./components/navbar"
-import './styles/searchPage.css'
+import './styles/home.css'
 import { searchMovies } from "./api/api";
 import Footer from "./components/footer";
 
