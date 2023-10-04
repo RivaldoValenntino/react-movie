47d95401f936c22ea4e71f4b609abb6625ff6666
Rivaldo Valentino <jefry.kotak1908@gmail.com>
2023-10-04 09:41:42 +0700

    fix routing

--- src/main.jsx
+++ src/main.jsx
@@ -1,34 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import { RouterProvider, createBrowserRouter } from 'react-router-dom';
-import Home from './Home.jsx'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './Home.jsx';
 import TvSeriesPage from './Series';
 import MovieListPage from './Film';
 import Intro from './Intro';
 
-
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <Intro />
-  },
-  {
-    path: '/all-movies',
-    element: <Home/>
-  },
-  {
-    path: '/list-tv',
-    element: <TvSeriesPage />
-  },
-  {
-    path: '/list-movies',
-    element: <MovieListPage />
-  }
-])
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router}/>
-  </React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Intro />} />
+        <Route path="/all-movies" element={<Home />} />
+        <Route path="/list-tv" element={<TvSeriesPage />} />
+        <Route path="/list-movies" element={<MovieListPage />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>,
 );
