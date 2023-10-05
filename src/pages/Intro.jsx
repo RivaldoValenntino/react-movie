5c6af5aac84aa17ef7770922c655b12036ecf244
Rivaldo Valentino <jefry.kotak1908@gmail.com>
2023-10-05 12:33:21 +0700

    fix slideshow

--- src/pages/Intro.jsx
+++ src/pages/Intro.jsx
@@ -9,7 +9,7 @@ const Intro = () => {
             <div className="search-page w-full flex flex-col justify-center items-center z-[999]">
                 <section className="mt-5">
                     <div className="pt-10">
-                        <h1 className="text-white lg:text-5xl text-center text-3xl font-bold" style={{ fontFamily: 'Poppins', textShadow: '2px 2px 3px #000000', letterSpacing: '2px' }}>Welcome to MovieApp</h1>
+                        <h1 className="text-white lg:text-5xl text-center text-3xl font-bold mb-2" style={{ fontFamily: 'Poppins', textShadow: '2px 2px 3px #000000', letterSpacing: '2px' }}>Welcome to MovieApp</h1>
                         <p className="text-white text-center lg:text-4xl text-xl font-semibold" style={{ fontFamily: 'Poppins', textShadow: '2px 2px 3px #000000', letterSpacing: '2px' }}>Discover every Movie and TV Show</p>
                         <p className="text-white text-center lg:text-4xl font-semibold" style={{ fontFamily: 'Poppins', textShadow: '2px 2px 3px #000000', letterSpacing: '2px' }}>From Trending and Popular</p>
                     </div>
